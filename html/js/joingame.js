ab08a390649249643ad1746425f394e33633396e
Bennett Warner <bennettdwarner@gmail.com>
2018-12-01 18:34:46 -0500

    dld

--- html/js/joingame.js
+++ html/js/joingame.js
@@ -2,24 +2,16 @@
 
 function join_game() {
     // $('#join_submit').on('click', function() {
-        var attendee_location = $('#att_location').val();
-        var type = $('#game_type').val();
         $.ajax({
-            url: 'api/event/list',
+            url: '/api/event/list',
             type: 'GET',
-            data: {
-                location: attendee_location,
-                game_type: type
-            },
             cache: false,
             timeout: 600000,
         }).done(function(data) {
-            // take back to login screen?
-            if (data['events'].length > 0) {
-                data['events'].forEach((event) => {
-                    $('#events').append(`<li>${event}<li>`)
-                });
-            }
+          console.log(data[0]);
+            //for (event in data) {
+            //    console.log(event);
+            //    };
 
         });
     // });
