b3112214d7f12e444a56af01b6ce42ffd03dd4ad
Bennett Warner <bennettdwarner@gmail.com>
2018-12-01 13:56:18 -0500

    dld

--- app/main.py
+++ app/main.py
@@ -1,7 +1,52 @@
-from bottle import route, run, template
+import bottle
+from bottle_jwt import (JWTProviderPlugin, jwt_auth_required)
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=8080)
+app = bottle.Bottle()
+
+server_secret = 'TEAMROCKET'
+
+
+class AuthBackend(object):
+    user = {'id': 1237832, 'username': 'ben', 'password': '123', 'data': {'sex': 'male', 'active': True}}
+
+    def authenticate_user(self, username, password):
+        """Authenticate User by username and password.
+
+        Returns:
+            A dict representing User Record or None.
+        """
+        if username == self.user['username'] and password == self.user['password']:
+            return self.user
+        return None
+
+    def get_user(self, user_id):
+        """Retrieve User By ID.
+
+        Returns:
+            A dict representing User Record or None.
+        """
+        if user_id == self.user['id']:
+            return {k: self.user[k] for k in self.user if k != 'password'}
+        return None
+
+
+provider_plugin = JWTProviderPlugin(
+    keyword='jwt',
+    auth_endpoint='/auth',
+    backend=AuthBackend(),
+    fields=('username', 'password'),
+    secret=server_secret,
+    ttl=30
+)
+
+app.install(provider_plugin)
+
+
+@app.get('/')
+@jwt_auth_required
+def private_resource():
+    return {"scope": "For your eyes only!", "user": bottle.request.get_user()}
+
+
+bottle.run(app=app, port=8080)
