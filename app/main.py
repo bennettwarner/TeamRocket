a1fc8cefe9a3280650a43d51743a0ff63a02d195
Bennett Warner <bennettdwarner@gmail.com>
2018-12-03 18:54:28 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,34 +1,26 @@
+# Written by Bennett Warner - Last updated 12/3/2018
+# Package Imports
 import bottle
-from bottle_jwt import (JWTProviderPlugin, jwt_auth_required)
-from tinydb import TinyDB, Query
 import random
+from tinydb import TinyDB, Query
+from bottle_jwt import (JWTProviderPlugin, jwt_auth_required)
+
 event_db = TinyDB('events.json')
-
 app = bottle.Bottle()
-
 server_secret = 'TEAMROCKET'
 
+
+#Auth Framework
 class AuthBackend(object):
-    user = {'id': 1237832, 'username': 'test', 'password': '123', 'data': {'sex': 'male', 'active': True}}
+    user = {'id': 1237832, 'username': 'test', 'password': '123'}
 
     def authenticate_user(self, username, password):
-        """Authenticate User by username and password.
-
-        Returns:
-            A dict representing User Record or None.
-        """
         print('Login attempt from: {}:{}'.format(username, password))
-
         if username == self.user['username'] and password == self.user['password']:
             return self.user
         return None
 
     def get_user(self, user_id):
-        """Retrieve User By ID.
-
-        Returns:
-            A dict representing User Record or None.
-        """
         if user_id == self.user['id']:
             return {k: self.user[k] for k in self.user if k != 'password'}
         return None
@@ -42,17 +34,42 @@ provider_plugin = JWTProviderPlugin(
     ttl=30
 )
 
+#Install JWT auth framework
 app.install(provider_plugin)
 
 
-@app.get('/authtest')
+#########################
+#### Event API Endpoint
+#########################
+# Methods:
+#/event/<eventID>
+#/event/list
+#/event/list/<username>
+#/event/create
+#event/delete/<eventID>
+
+@app.get('/api/event/<id>')
 @jwt_auth_required
-def private_resource():
-    return {"Auth": "True"}
+def get_events(id):
+    event = Query()
+    return str(event_db.search(event.id == id))
+
+
+@app.get('/api/event/list')
+@jwt_auth_required
+def get_events():
+    return str(event_db.all())
+
+
+@app.get('/api/event/list/<user>')
+@jwt_auth_required
+def get_events(user):
+    event = Query()
+    return str(event_db.search(event.user == user))
 
 
 @app.post('/api/event/create')
-#@jwt_auth_required
+@jwt_auth_required
 def create_event():
     name = bottle.request.forms.get('name')
     game = bottle.request.forms.get('game')
@@ -65,26 +82,8 @@ def create_event():
     return {'Success': 'Event Created'}
 
 
-@app.get('/api/event/<id>')
-#@jwt_auth_required
-def get_events(id):
-    event = Query()
-    return str(event_db.search(event.id == id))
-
-
-@app.get('/api/event/list')
-#@jwt_auth_required
-def get_events():
-    return str(event_db.all())
-
-@app.get('/api/event/list/<user>')
-#@jwt_auth_required
-def get_events(user):
-    event = Query()
-    return str(event_db.search(event.user == user))
-
 @app.route('/api/event/delete/<event_id>')
-#@jwt_auth_required
+@jwt_auth_required
 def delete(event_id):
     event = Query()
     find_event = event_db.search(event.id == int(event_id))
@@ -95,15 +94,23 @@ def delete(event_id):
         return {'Success': 'Event Removed'}
 
 
+    
+############################################    
+#### Static file hosting
+############################################
 @app.route('/')
 def server_static():
     return bottle.static_file('index.html', root='../html')
+
+
 @app.route('/<filename>')
 def server_static(filename):
     return bottle.static_file(filename, root='../html')
+
+
 @app.route('/js/<filename>')
 def server_static(filename):
     return bottle.static_file(filename, root='../html/js')
 
-
+#Run App
 bottle.run(app=app, port=8080)
